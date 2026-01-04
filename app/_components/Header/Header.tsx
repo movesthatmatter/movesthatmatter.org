123b1e69d6d0a8481149c788c23ac91784965ebe
Gabriel C. Troia <catalin.troia@gmail.com>
2026-01-04 02:40:55 +0200

    Add manifesto, ideas, actions and blog sections. Add content as well

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -4,8 +4,22 @@ export default function Header() {
   return (
     <header className="flex pb-4 text-black">
       <Logo />
-      <div className="flex flex-1 items-center align-center sbg-red-100 pl-4 items-end justify-end">
-        <a href="/manifesto" className='hover:font-bold'>Manifesto</a>
+      <div className="flex flex-1 items-center align-center sbg-red-100 pl-4 items-end justify-end gap-4">
+        <a href="/manifesto" className="hover:font-bold hover:underline">
+          Manifesto
+        </a>
+        <a href="/inspiration" className="hover:font-bold hover:underline">
+          Inspiration
+        </a>
+        <a href="/ideas" className="hover:font-bold hover:underline">
+          Ideas
+        </a>
+        <a href="/actions" className="hover:font-bold hover:underline">
+          Actions
+        </a>
+        <a href="/blog" className="hover:font-bold hover:underline">
+          Blog
+        </a>
       </div>
     </header>
   );
---
+++ app/ideas/(pages)/layout.tsx
@@ -0,0 +1,23 @@
+import Header from '@/app/_components/Header/Header';
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col min-h-screen bg-zinc-50 font-mono p-6">
+      <Header />
+      <main className="mx-auto min-h-screen w-full py-8 text-black">
+        <div
+          className="prose prose-lg sdark:prose-invert
+              prose-h1:font-bold prose-h1:text-xl
+              prose-a:text-blue-600 prose-p:text-justify prose-img:rounded-xl
+              sprose-headings:underline"
+        >
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+}
