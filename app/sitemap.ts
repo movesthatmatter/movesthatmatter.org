1b54a9963d1eeff96e481b2929a84aba739e6e23
Gabriel C. Troia <catalin.troia@gmail.com>
2026-01-09 22:32:08 +0200

    Add all the pages to the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,24 @@
-// import { getBlogPosts } from 'app/blog/utils'
-
 import { getBlogPosts } from './blog/_util';
+import { getContributors } from './contributors/_util';
 import { getIdeas } from './ideas/_util';
 
 export default async function sitemap() {
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-  let blogs = getBlogPosts().map((post) => ({
+  const routes = [
+    '',
+    '/manifesto',
+    '/ideas',
+    '/actions',
+    '/inspiration',
+    '/blog',
+    '/contributors',
+  ].map((route) => ({
+    url: `${BASE_URL}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+
+  const blogs = getBlogPosts().map((post) => ({
     url: `${BASE_URL}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }));
@@ -16,10 +28,10 @@ export default async function sitemap() {
     lastModified: post.metadata.publishedAt,
   }));
 
-  let routes = ['', '/blog', '/ideas'].map((route) => ({
-    url: `${BASE_URL}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
+  const contributors = getContributors().map((post) => ({
+    url: `${BASE_URL}/ideas/${post.slug}`,
+    // lastModified: post.metadata.publishedAt,
   }));
 
-  return [...routes, ...blogs, ...ideas];
+  return [...routes, ...blogs, ...ideas, ...contributors];
 }
